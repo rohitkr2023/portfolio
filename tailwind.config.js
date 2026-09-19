2585f4bc463b94bbccf8abc665bcbd552467f71f
rohitkr2023 <rohitkr2023@gmail.com>
2026-09-19 16:06:08 +0530

    fix: tailwind setup for windows

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,13 @@
 ﻿/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      animation: {
-        marquee: "marquee 25s linear infinite",
-      },
-      keyframes: {
-        marquee: {
-          "0%": { transform: "translateX(0%)" },
-          "100%": { transform: "translateX(-50%)" },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
